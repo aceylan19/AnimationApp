4bb85202e4c89c60871e83f816f4dfe9a89cb4d7
Aleyna Ceylan <aceylan1@binghamton.edu>
2021-10-05 00:37:36 -0400

    Project files uploaded.

---
+++ animation_Trials/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animation_Trials
+//
+//  Created by Aleyna  Ceylan on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //
+    }
+
+
+}
+
